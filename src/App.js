27678e2b32a5bf1539d4766660f9d0b5cbebc09b
ANASFARHANE <anasfarhane321@gmail.com>
2022-07-08 14:40:39 +0100

    sdq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Person = (props)=>{
+    return (
+        <>
+            <h1>Name : {props.name}</h1>
+            <h2>Last Name : {props.lastName}</h2>
+            <h2>Age : 30</h2>
+        </>
+    )
+}
+
+
+const App = () =>{
+    const name = 'John';
+    const isNameShowing = true;
+    return (
+        <div className="App">
+            <h1> Hello, {isNameShowing ? name: 'World'}! </h1>
+            {
+                name ? (
+                    <>
+                    <Person name='John' lastName='Doe'/>
+                    <Person name='oppa' lastName='nuna'/>
+                    </>
+                ):(
+                    <>
+                    <h1>test</h1>
+                    <h2>There is no name</h2>
+                    </>
+                    )
+            }
+        </div>
+);
 }
 
 export default App;
